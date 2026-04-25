012f2ed6b66751927277e59e66324f44fd8f8c76
Reissel Reis <reisreissel@gmail.com>
2026-04-25 20:18:38 -0300

    Ajustado modelo de notificação para validação de body na rota de POST.

--- model/notification.go
+++ model/notification.go
@@ -2,15 +2,15 @@ package model
 
 type Notification struct {
 	ID              int    `json:"id"`
-	Chamado_id      string `json:"chamado_id"`
-	Tipo            string `json:"tipo"`
-	Cpf             string `json:"cpf,omitempty"`
+	Chamado_id      string `json:"chamado_id" binding:"required"`
+	Tipo            string `json:"tipo" binding:"required"`
+	Cpf             string `json:"cpf,omitempty" binding:"required"`
 	Cpf_encrypted   string `json:"-"`
 	Cpf_bindex      string `json:"-"`
-	Status_anterior string `json:"status_anterior"`
-	Status_novo     string `json:"status_novo"`
-	Titulo          string `json:"titulo"`
-	Descricao       string `json:"descricao"`
-	Timestamp       string `json:"timestamp"`
+	Status_anterior string `json:"status_anterior" binding:"required"`
+	Status_novo     string `json:"status_novo" binding:"required"`
+	Titulo          string `json:"titulo" binding:"required"`
+	Descricao       string `json:"descricao" binding:"required"`
+	Timestamp       string `json:"timestamp" binding:"required"`
 	Is_read         bool   `json:"is_read"`
 }
